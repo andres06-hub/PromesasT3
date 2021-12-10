d3848d36cfa79f7f325902ffef4e905eb56e0821
AndresSierra <andressierrarojas7@gmail.com>
2021-12-10 14:52:18 -0500

    Ejer2

--- ejer1.ts
+++ ejer1.ts
@@ -12,24 +12,3 @@ promesaUno.then((res) =>{
     console.log("--> "+res);
     
 })
-// _----------------------------------------------------------
-
-/**
- * Cree y ejecute una promesa que siempre se rechace, y que lo haga con un string
- * “Ha ocurrido un error desconocido.”, 
- * de tal manera que en su .catch, se imprima tal string mediante console.log
- */
-
-let promesaDos = new Promise((resolve, reject) => {
-    reject("Ha ocurrido un error desconocido.");
-});
-
-promesaDos.then((res) => {
-    console.log("No entrara ya que siempre sera un una rechasada");
-    
-}).catch((err) => {
-    console.log(err);
-    
-});
-
-
---
+++ ejer2.ts
@@ -0,0 +1,19 @@
+// _----------------------------------------------------------
+
+/**
+ * Cree y ejecute una promesa que siempre se rechace, y que lo haga con un string
+ * “Ha ocurrido un error desconocido.”, 
+ * de tal manera que en su .catch, se imprima tal string mediante console.log
+ */
+
+let promesaDos = new Promise((resolve, reject) => {
+    reject("Ha ocurrido un error desconocido.");
+});
+
+promesaDos.then((res) => {
+    console.log("No entrara ya que siempre sera un una rechasada");
+    
+}).catch((err) => {
+    console.log(err);
+    
+});
